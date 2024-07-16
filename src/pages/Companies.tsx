2f4afeb5667f9280b5312c860432cf59804ee037
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-16 19:10:52 +0100

    features: display all companies in table format

---
+++ src/pages/Companies.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import baseUrl from "@/apis/baseUrl";
+import { Table, TableBody, TableCaption } from "@/components/ui/table";
+import TableHeader from "@/components/ui/tables/TableHeader";
+import TableFooter from "@/components/ui/tables/TableFooter";
+import TableBodyRow from "@/components/ui/tables/TableBody";
+
+const Companies = () => {
+  const [business, setBusiness] = useState<any>([]);
+  const [isLoading, setisLoading] = useState(true);
+  const getData = async () => {
+    try {
+      const response = await baseUrl.get("/vendors");
+      setBusiness(response.data);
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      getData();
+      setisLoading(false);
+    }
+  }, [isLoading, business]);
+
+  return isLoading ? (
+    <div>loading...</div>
+  ) : (
+    <Table>
+      <TableCaption>A list of Businesses.</TableCaption>
+      <TableHeader />
+      <TableBody>
+        {business?.data?.map((item: any) => (
+          <TableBodyRow item={item} key={item.id} />
+        ))}
+      </TableBody>
+      {isLoading === false && <TableFooter />}
+    </Table>
+  );
+};
+export default Companies;
